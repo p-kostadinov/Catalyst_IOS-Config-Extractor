84381b627975e923d6efb14baf48a8fdd10e3436
Pavel Kostadinov <153768067+p-kostadinov@users.noreply.github.com>
2025-01-25 11:18:41 -0600

    Update format_config.sh
    improved script readability

--- format_config.sh
+++ format_config.sh
@@ -3,7 +3,7 @@
 #Cleans up running config output from 'show run | i interfaces'
 #Dump log file (rename to .txt) into ./imports directory and run script to clean up file and prepare for extractor.
 #You will want to modify the second pass and its target. Modify which column awk uses and the term to find 
-#so the script can terminate the list of interfaces(lines 26/29)
+#so the script can terminate the list of interfaces(lines 28/30 and 32/34)
 
 if [ ! -d ./configs ]; then
     mkdir ./configs
@@ -13,24 +13,24 @@ mapConfig() {
     mapfile -t -O 0 findList < ./find.txt ; rm -f ./find.txt
 }
 
-for file in ./import/*; do
-    awk '{print $1}' "$file" > ./find.txt
-    mapConfig
+loopFileAndFind() {
     for i in $(seq 0 ${#findList[@]} ); do
-        if [ "${findList["$i"]}" = "interface" ]; then
+        if [ "${findList["$i"]}" = "$1" ]; then
             chopAmount=$i
             break
         fi
     done
+}
+
+for file in ../import/*; do
+    awk '{print $1}' "$file" > ./find.txt
+    mapConfig
+    loopFileAndFind "interface"
     sed -i '' "1,${chopAmount}d" "${file}"
     awk '{print $2}' "$file" > ./find.txt
     mapConfig
-    for i in $(seq 0 ${#findList[@]} ); do
-        if [ "${findList["$i"]}" = "http" ]; then
-            chopAmount=$((i + 1))
-            break
-        fi
-    done
+    loopFileAndFind "http"
+    ((chopAmount++))
     sed -i '' "${chopAmount},${#findList[@]}d" "${file}"
     sed -i '' 's/interface/!\ninterface/g' "$file"
     echo "!" >> "$file"
